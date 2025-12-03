836b0199327f6a0ea6d748ba0980f1476858e85d
IAmKirbki <daimywisselink91@gmail.com>
2025-12-03 16:12:44 +0100

    Fix: comment out console log in User model method

--- src/abstract/User.ts
+++ src/abstract/User.ts
@@ -2,6 +2,6 @@ import Model from "./Model";
 
 export default class User extends Model<{ id: string; name: string; }> {
     public someMethod(): void {
-        console.log("This is a method in the User model.");
+        // console.log("This is a method in the User model.");
     }
 }
