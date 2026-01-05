9ceb0ebf835ecd906ad52ae168e33ab6eaccbcde
IAmKirbki <daimywisselink91@gmail.com>
2026-01-05 09:34:11 +0100

    Chore: Removal of the packages to use git submodules

--- packages/pg/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as PostgresAdapter } from "./PostgresAdapter.js";
-
-export { PostgresSchemaBuilder } from "./PostgresSchemaBuilder.js";
-export { PostgresTableSchemaBuilder } from "./PostgresTableSchemaBuilder.js";
